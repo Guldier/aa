be6b1b1c3ab98ba1cccc61a52d2069ff2e6def00
Guldier <damianjadacki@gmail.com>
2022-08-15 19:14:00 +0200

    pic change

--- UnderlinerApp/settings.py
+++ UnderlinerApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['0.0.0.0']#['.herokuapp.com']
 
 
 # Application definition
